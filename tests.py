712207a33b2510f58523929cee8b41b3920f5dfc
Alphaman1321 <abouditaniic@gmail.com>
2026-03-12 06:04:46 +0200

    API calls

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ API_URL = "https://nopd4hs5hi.execute-api.us-east-2.amazonaws.com/Proj/post-meal
 with open("food.jpg", "rb") as f:
     img = base64.b64encode(f.read()).decode()
 
-payload = {
+payload = { #too lazy to make it not require a user, so ppl hav to sign in to upload something, i need to add a users db tho
     "username": "abdullah",
     "image": img
 }
