2f338ff558291023d47f5a30f5f03e0a6822452b
Alphaman1321 <abouditaniic@gmail.com>
2026-03-13 09:55:20 +0200

    ss

--- testsforMealRec.py
+++ testsforMealRec.py
@@ -3,7 +3,8 @@ import requests
 url = "https://nopd4hs5hi.execute-api.us-east-2.amazonaws.com/Proj/meal-recommender"
 
 payload = {
-    "username": "abdullah"
+    "username": "abdullah",
+    "query": "high protein suhoor meal"
 }
 
 res = requests.post(url, json=payload)
